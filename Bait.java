cfe2d2fa15e04268cad87f75b1153bb837985e00
Manu <manukorupolu@gmail.com>
2023-05-27 15:43:43 -0700

    Add files via upload

---
+++ Bait.java
@@ -0,0 +1,17 @@
+/*
+ * Activity 4.9.3
+ */
+public class Bait extends LakeObject
+{
+    @Override
+    public String say()
+    {
+        return "You now have bait!";
+    }
+    @Override
+    public boolean wasCaught()
+    {
+        return true;
+    }
+
+}
---
+++ Boot.java
@@ -0,0 +1,11 @@
+/*
+ * Activity 4.9.3
+ */
+public class Boot extends LakeObject
+{
+    @Override
+    public String say()
+    {
+        return "You now have a boot!";
+    }
+}
